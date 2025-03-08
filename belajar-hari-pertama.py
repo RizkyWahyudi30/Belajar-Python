2e7509edb1225bc36f2ec1a9dd3eb7c5f260c133
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-03-08 18:03:47 +0700

    belajar-pertama

---
+++ belajar-hari-pertama.py
@@ -0,0 +1,67 @@
+# HALO SELAMAT DATANG DI GITHUB AKU
+# DISINI KITA AKAN BELAJAR BAHASA PYTHON BERSAMA SAMA
+
+# PADA HARI PERTAMA INI, KITA AKAN BELAJAR BEBERAPA HAL DASAR PADA PYTHON
+
+# KITA AKAN BELAJAR MENGENAI
+# 1. SYNTAX DASAR PYTHON 
+# 2. INDENTATION PADA PYTHON
+# 3. COMMENTS DI PYTHON
+
+# OKE KARENA SEMUA NYA UDAH KITA SUSUN, MARI KITA BELAJAR
+
+# KITA AKAN BELAJAR DARI YANG AWAL YA
+
+# 1. syntax dasar pada bahasa python
+# nah kita mulai dari syntax dulu nih, syntax itu apasih?
+# Syntax atau sintaksis adalah aturan yang mengatur cara menyusun kata, simbol, 
+# atau angka untuk membentuk kalimat atau kode program
+# contohnya
+
+print("halo nama saya rizky wahyudi")
+# ini merupakan salah satu dari contoh syntax
+
+a = 2
+print(a)
+# ini juga merupakan salah satu contoh dari syntax
+
+
+
+# 2. indentation pada python
+# indentation itu apasih? identation atau lekukan dapat diartikan sebagai tata letak dalam mengetikkan kode
+# indentation itu biasanya pada penggunaan pengkondisian dan yang lainnya
+# contohnya
+
+if 5 > 2:
+    print("YES THATS RIGHT")
+# dimana letak indentation nya? letak nya pada baris print yang lebih maju dari baris if / tidak sejajar
+# bagaimana jika sejajar?
+
+# jika syntax nya seperti ini :
+#           if 5 > 2:
+#           print("YES THATS RIGHT")
+
+# akan terjadi eror / false, karena titik dua ( : ) itu mengartikan bahwa "print("YES THATS RIGHT")" merupakan bagian dari pengkondisian if
+# jika dibawahnya, maka sistem akan membaca kedua baris memiliki nilai sendiri sendiri
+
+
+
+# 3. comments di python
+# comments di python itu maksudnya apasih?
+# comments di python seperti petunjuk untuk kode, maksud petunjuk? maksudnya sebagai penanda di setiap kode yang ingin kita beri petunjuk
+
+# contoh dari comment itu gimana sih?
+
+# YA INI ADALAH CONTOH DARI COMMENTSS 😘😘😘
+# simbol pagar ini berfungsi sebagai comments
+# comments tidak akan ditampilkan ketika kita menjalankan kode yang kita tulis
+
+# contohnya :
+
+# ini syntax print
+print("lol")
+# baris yang berisi kalimat "ini syntax print" tidak akan muncul ketika di running / dijalankan kode nya
+
+
+
+# MUNGKIN CUKUP UNTUK HARI PERTAMA DALAM PYTHON INI, KITA AKAN LANJUT KESERUANNYA DI HARI KEDUA YAH KAWAN KAWAN
