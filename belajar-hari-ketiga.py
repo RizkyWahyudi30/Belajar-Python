295af55e94044d4917677be7ed9cf6d632b71ace
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-03-10 09:36:14 +0700

    belajar python hari ketiga

---
+++ belajar-hari-ketiga.py
@@ -0,0 +1,51 @@
+# HALO KAWAN KAWAN INI UDAH HARI KETIGA NIH KITA BELAJAR PYTHON BARENG BARENG !!
+# SEKARANG KITA LANJUT YUK MATERI NYA
+
+# KEMARIN KITA UDAH BAHAS TENTANG VARIABLES KAN, NAH MATERI HARI INI MASIH SAMA SIH SAMA KAYAK KEMARIN, TENTANG VARIABLES
+# TAPI KALO KEMAREN MATERI NYA BELUM LEBIH RINCI, SEKARANG KITA RINCIIN NIH, YUK KITA PELAJARIN !! 
+
+# SEKARANG KITA AKAN MEMBAHASA TENTANG VARIABLES NAME ATAU ATURAN DALAM MEMBUAT NAMA VARIABLES
+
+# DALAM MEMBUAT NAMA VARIABLE ADA ATURAN ATURAN NYA LOH
+
+# dalam membuat nama variabel, kita bisa menggunakan yang singkat atau yang spesifik
+
+# aturan aturan
+
+'''
+1. Nama variabel harus diawali dengan huruf atau karakter garis bawah
+2. Nama variabel tidak dapat diawali dengan angka
+3. Nama variabel hanya dapat berisi karakter alfanumerik dan garis bawah (A-z, 0-9, dan _)
+4. Nama variabel peka huruf besar-kecil (age, Age, dan AGE adalah tiga variabel yang berbeda)
+5. Nama variabel tidak dapat berupa kata kunci Python.
+'''
+
+'''
+contoh dari nama variabel yang legal :
+1. varsatu = "Wahyu"
+2. var_satu = "Wahyu"
+3. _var_satu = "Wahyu"
+4. varSatu = "Wahyu"
+5. VARSATU = "Wahyu"
+6. varsatu2 = "Wahyu"
+
+contoh dari nama variabel yang ilegal :
+1. 1varsatu
+2. var-satu
+3. var satu
+
+ini dinamakan dengan case-sensitive
+'''
+
+# didalam penamaan variabel, juga ada nama - nama nya sendiri
+'''
+1. Camel Case ==> biasanya huruf pertama dari kalimat kedua besar, begitupun selanjutnya, yang kecil hanya kalimat yang depannya saja
+   contoh ==> namaDepanOrang
+
+2. Pascal Case ==> jika pascal case maka semua awal kalimat nya diawali huruf besar 
+   contoh ==> NamaDepanOrang
+
+3. Snake Case ==> ini menggunakan strip bawah _ pada penggunaan nama variabel
+   contohnya ==> nama_depan_orang
+'''
+
